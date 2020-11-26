a50987f3b5206a41f6f04f517ff22abdc521f4d5
Roger Mähler <roger.mahler@hotmail.com>
2020-11-26 07:49:57 +0100

    Adapted to penelope changes

--- tests/test_data/utils.py
+++ tests/test_data/utils.py
@@ -1,7 +1,8 @@
 import os
-from typing import Callable, Sequence
+from typing import Callable, List, Sequence
 
 from penelope.corpus.readers import TextTokenizer
+from penelope.corpus.readers.interfaces import TextReaderOpts
 from penelope.corpus.readers.text_transformer import TextTransformOpts
 from penelope.utility import IndexOfSplitOrCallableOrRegExp
 
@@ -17,25 +18,36 @@ if __file__ in globals():
 
 def create_text_tokenizer(
     source_path=TEST_CORPUS_FILENAME,
-    transforms=None,
-    chunk_size: int = None,
+    # TextTransformOpts
+    fix_whitespaces: bool=False,
+    fix_hyphenation: bool=True,
+    extra_transforms: List[Callable]=None,
+    # TextReaderOpts
+    as_binary: bool = False,
     filename_pattern: str = "*.txt",
     filename_filter: str = None,
-    fix_whitespaces=False,
-    fix_hyphenation=True,
-    as_binary: bool = False,
-    tokenize: Callable = None,
     filename_fields: Sequence[IndexOfSplitOrCallableOrRegExp] = None,
+    filename_fields_key: str=None,
+    # TokenizeOpts:
+    tokenize: Callable = None,
+    chunk_size: int = None,
 ):
-    kwargs = dict(
-        transforms=transforms,
-        chunk_size=chunk_size,
+    text_transform_opts = TextTransformOpts(
+        fix_whitespaces=fix_whitespaces, fix_hyphenation=fix_hyphenation, extra_transforms=extra_transforms
+    )
+    reader_opts = TextReaderOpts(
         filename_pattern=filename_pattern,
         filename_filter=filename_filter,
-        text_transform_opts=TextTransformOpts(fix_whitespaces=fix_whitespaces, fix_hyphenation=fix_hyphenation),
-        as_binary=as_binary,
-        tokenize=tokenize,
         filename_fields=filename_fields,
+        filename_fields_key=filename_fields_key,
+        as_binary=as_binary,
     )
-    reader = TextTokenizer(source_path, **kwargs)
+
+    reader = TextTokenizer(
+        source=source_path,
+        text_transform_opts=text_transform_opts,
+        reader_opts=reader_opts,
+        chunk_size=chunk_size,
+        tokenize=tokenize
+    )
     return reader
