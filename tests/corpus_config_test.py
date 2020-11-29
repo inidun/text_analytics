5ded0cdc27020ea24a013cefb9d4504145ef8731
Roger Mähler <roger.mahler@hotmail.com>
2020-11-29 20:46:29 +0100

    Added tests

---
+++ tests/corpus_config_test.py
@@ -0,0 +1,18 @@
+from penelope.corpus.readers.interfaces import TextReaderOpts
+from penelope.pipeline import PipelinePayload, CorpusConfig
+
+
+def test_corpus_config_set_folder():
+
+    config: CorpusConfig = CorpusConfig(
+        text_reader_opts=TextReaderOpts(),
+        pipeline_payload=PipelinePayload(
+            source="corpus.zip",
+            document_index_source="document_index.csv",
+        ),
+    )
+
+    config.set_folder('/data')
+
+    assert config.pipeline_payload.source == '/data/corpus.zip'
+    assert config.pipeline_payload.document_index_source == '/data/document_index.csv'
--- tests/word_endings_test.py
+++ tests/word_endings_test.py
@@ -1,58 +1,24 @@
-import numpy as np
-import pytest
-import textacy
-from penelope.corpus import CorpusVectorizer, VectorizedCorpus
-from penelope.vendor import textacy as textacy_utility
+import os
 
+from penelope.corpus.readers import TextTransformOpts
+from penelope.pipeline import SpacyPipeline
 
-@pytest.fixture(scope="module")
-def mary_had_a_little_lamb_corpus() -> textacy.Corpus:
-    """Source: https://github.com/chartbeat-labs/textacy/blob/master/tests/test_vsm.py """
-    texts = [
-        "Mary had a little lamb. Its fleece was white as snow.",
-        "Everywhere that Mary went the lamb was sure to go.",
-        "It followed her to school one day, which was against the rule.",
-        "It made the children laugh and play to see a lamb at school.",
-        "And so the teacher turned it out, but still it lingered near.",
-        "It waited patiently about until Mary did appear.",
-        "Why does the lamb love Mary so? The eager children cry.",
-        "Mary loves the lamb, you know, the teacher did reply.",
-    ]
-    corpus = textacy.Corpus("en", data=texts)
-    return corpus
+from penelope.pipeline import CorpusConfig
+from notebooks.corpus_data_config import SSI
 
+CORPUS_FOLDER = './tests/test_data'
+OUTPUT_FOLDER = './tests/output'
 
-def test_word_endings(mary_had_a_little_lamb_corpus):  # pylint: disable=redefined-outer-name
+def test_create_pos_checkpoint_pipeline() -> SpacyPipeline:
 
-    expected_matrix = np.matrix([[0, 0], [0, 0], [1, 0], [0, 1], [0, 0], [0, 0], [0, 0], [0, 0]])
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-    # Arrange
-    terms = (
-        textacy_utility.ExtractPipeline(mary_had_a_little_lamb_corpus, target=None).min_character_filter(2).process()
-    )
-    document_terms = ((f'document_{i}.txt', tokens) for i, tokens in enumerate(terms))
-    vectorizer = CorpusVectorizer()
-    v_corpus: VectorizedCorpus = vectorizer.fit_transform(document_terms, tokenizer=None)
+    data_folder: str = CORPUS_FOLDER
+    ssi: CorpusConfig = SSI.set_folder(data_folder)
+    checkpoint_filename: str = os.path.join(OUTPUT_FOLDER, 'ssi_pos_csv.zip')
 
-    vocabulary = list(v_corpus.token2id.keys())
+    #pipeline = create_pos_checkpoint_pipeline(config=ssi, checkpoint_filename=checkpoint_filename)
 
-    word_ending = 'ay'
+    #pipeline.exhaust()
 
-    candidates = {x for x in vocabulary if x.endswith(word_ending)}
-
-    w_corpus = v_corpus.slice_by(lambda w: w in candidates)
-
-    assert (expected_matrix == w_corpus).all()
-    assert {0: 'day', 1: 'play'} == w_corpus.id2token
-
-
-"""
-Add an ability to enter a number of specific word endings such as -ment, -tion and -sion.
-The system should finds all words having the specified endings, and displays the (optionally normalized)
-frequency list as a table, or bar chart. It should also be possible to export the list, and to a apply a
-filter that excludes any number of words (in a text box).
-
-It should be possible to display the data grouped by document, year or user defined periods.
-
-See Moretti, Pestre Bank Speek, page 89
-"""
+    assert os.path.isfile(checkpoint_filename)
