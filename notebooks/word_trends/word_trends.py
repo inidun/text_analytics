5a4382fa28a6288bd788a43d41c0d5723bb80328
Roger Mähler <roger.mahler@hotmail.com>
2020-12-10 07:46:10 +0100

    Word trends GUI

--- notebooks/word_trends/word_trends.py
+++ notebooks/word_trends/word_trends.py
@@ -8,48 +8,84 @@
 #       format_version: '1.3'
 #       jupytext_version: 1.6.0
 #   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
+#     display_name: 'Python 3.8.5 64-bit (''text-analytics-symNAVlf-py3.8'': venv)'
+#     metadata:
+#       interpreter:
+#         hash: f9f5370a66fe709225320b0a5e42eaf481fec3c92c4ac457a33063cd6d0e2297
+#     name: 'Python 3.8.5 64-bit (''text-analytics-symNAVlf-py3.8'': venv)'
 # ---
 
-# %% [markdown]
-# # Setup Notebook
-
-# %% tags=[] vscode={}
-# %load_ext autoreload
-# %autoreload 2
-
-import __paths__  # isort:skip
-
-# %% [markdown]
-# ### Load and display corpus
-import importlib
-
-# pylint: disable=wrong-import-order, no-member
-import os
-
-import notebooks.word_trends.word_trends_compute_gui as vectorize_corpus_gui
-import penelope.notebook.vectorized_corpus_load_gui as load_corpus_gui
-from bokeh.plotting import output_notebook
-from penelope.notebook.word_trends import display_word_trends
-
-root_folder = __paths__.ROOT_FOLDER
-corpus_folder = os.path.join(root_folder, 'data')
-output_notebook(hide_banner=True)
-
-
-importlib.reload(vectorize_corpus_gui)
-
-filename_fields = ["unesco_id:_:2", "year:_:3", r'city:\w+\_\d+\_\d+\_\d+\_(.*)\.txt']
-year_range = [1945, 2020]
-
-vectorize_corpus_gui.display_gui(
-    filename_fields=filename_fields,
-    corpus_folder=corpus_folder,
-    year_range=year_range,
-    display_callback=display_word_trends,
-)
 
 # %%
-load_corpus_gui.display_gui(loaded_callback=display_word_trends)
+
+from dataclasses import dataclass
+
+import penelope.notebook.dtm as vectorize_gui
+import penelope.notebook.dtm.load_DTM_gui as load_DTM_gui
+import penelope.notebook.word_trends as word_trends
+from bokeh.plotting import output_notebook
+from IPython.core.display import display
+from penelope.corpus import VectorizedCorpus
+
+import __paths__
+from notebooks.common.spacy_pipelines import spaCy_DTM_pipeline
+from notebooks.corpus_data_config import SSI, CorpusConfig
+
+output_notebook()
+
+
+@dataclass
+class State:
+    word_trend_data: word_trends.WordTrendData = None
+
+
+ssi: CorpusConfig = SSI(corpus_folder=__paths__.data_folder)
+state = State(word_trend_data=word_trends.WordTrendData())
+
+
+def corpus_loaded_callback(
+    corpus: VectorizedCorpus, corpus_tag: str, corpus_folder: str
+):  # pylint: disable=unused-argument
+    global state
+    print("Corpus succesfully vectorized!")
+    print("Generating trend data!")
+    state.word_trend_data.update(
+        corpus=corpus.group_by_year(),
+        corpus_folder=corpus_folder,
+        corpus_tag=corpus_tag,
+        n_count=1000,
+    )
+    print("Data successfully created!")
+
+
+# %%
+
+gui_vectorize = vectorize_gui.display_gui(
+    corpus_folder=__paths__.data_folder,
+    corpus_config=ssi,
+    pipeline_factory=spaCy_DTM_pipeline,
+    done_callback=corpus_loaded_callback,
+)
+
+display(gui_vectorize.layout(hide_input=False, hide_output=False))
+
+# %%
+
+gui_load = load_DTM_gui.create_gui(
+    corpus_folder=__paths__.data_folder,
+    loaded_callback=corpus_loaded_callback,
+)
+
+display(gui_load.layout())
+
+# %%
+
+gui_trends = word_trends.create_gui(
+    corpus=state.word_trend_data.corpus,
+    corpus_folder=state.word_trend_data.corpus_folder,
+    corpus_tag=state.word_trend_data.corpus_tag,
+    word_trend_data=state.word_trend_data,
+)
+display(gui_trends.layout())
+
+# %%
