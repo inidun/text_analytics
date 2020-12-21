1edc488d5e3b7ea43c5cf2c2017e617c30020e62
Roger Mähler <roger.mahler@hotmail.com>
2020-12-21 21:50:14 +0100

    Cleaner ipynb + documentation

--- notebooks/word_trends/word_trends.py
+++ notebooks/word_trends/word_trends.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -6,86 +7,83 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.6.0
+#       jupytext_version: 1.7.1
 #   kernelspec:
-#     display_name: 'Python 3.8.5 64-bit (''text-analytics-symNAVlf-py3.8'': venv)'
-#     metadata:
-#       interpreter:
-#         hash: f9f5370a66fe709225320b0a5e42eaf481fec3c92c4ac457a33063cd6d0e2297
-#     name: 'Python 3.8.5 64-bit (''text-analytics-symNAVlf-py3.8'': venv)'
+#     display_name: Python 3
+#     language: python
+#     name: python3
 # ---
 
+# %% [markdown]
+# ## Word, or part-of-word, trend analysis
+#
+# ### Text Processing Pipeline
+#
+# | | Building block | Arguments | Description |
+# | -- | :------------- | :------------- | :------------- |
+# | ⚙ | <b>SetTagger</b>SpacyModel | 'en' | spaCy as PoS tagger
+# | 📜| <b>LoadText</b> | reader_opts, transform_opts | Text stream provider
+# | 🔎 | <b>Tqdm</b> | ⚪ | Progress indicator
+# | ⌛ | <b>Passthrough</b> | ⚪ | Passthrough
+# | 🔨 | Spacy<b>ToTagged</b>Frame | ⚪ | PoS tagging
+# | 💾 | <b>Checkpoint</b> | checkpoint_filename | Checkpoint (tagged frames) to file
+# | 🔨 | TaggedFrame<b>ToTokens</b> | extract_tagged_tokens_opts, filter_opts | Tokens extractor
+# | 🔨 | <b>TokensTransform</b> | tokens_transform_opts | Tokens transformer
+# | 🔨 | <b>ToDocumentContentTuple</b> | ⚪ | Protocol/API adapter
+# | 🔎 | <b>Tqdm</b> | ⚪ | Progress indicator
+# | 🔨 | <b>ToDTM</b> | vectorize_opts| DTM vectorizer
+# | 💾 | <b>Checkpoint</b> | checkpoint_filename| Checkpoint (DTM) to file
+#
+# ### User instructions
+#
+# #### Compute DTM
+#
+# This notebook implements the entire processing pipeline from plain text to computed (and stored)
+# document-term matrix that are the basis for the word trend exploration.
+#
+# For large corpora the DTM processing time can be considerable and in such a case you
+# should consider using the CLI-version of the processing pipeline.
+#
+# Note that the computed DTM is saved on disk in the specified folder. You must enter a
+# tag that will be used when namin the (principel) result data file. This file will be named
+# tag + `_vectorized_data.pickle` and uniquely identifies the bundle of files that makes
+# up the result. Note that if the `tag` already exists in specified folder then it will will
+# be overwritten. The tag can be used to describe the main compute arguments. If the
+# The result files will be stored in a subfolder named with the tag if the `Create folder` option is checked.
+#
+# | | Config element |  Description |
+# | -- | :------------- | :------------- |
+# | | Corpus type | Type of corpus, disabled since only text corpora are allowed in this notebook.
+# | | Source corpus file | Select file (ZIP) or folder that contains the text documents.
+# | | Output tag | String that will be prefix to result files. Only valid filename chars allowed.
+# | | Output folder | Target folder for result files.
+# | | Part-of-speech groups | Groups of tags to include in DTM given corpus PoS-schema
+# | | Remove stopwords | Remove common stopwords using NLTK language specific stopwords
+# | | Extra stopwords | Additional stopwords
+# | | Filename fields | Specifies attribute values to be extracted from filenames
+#
+# N.B. Note that PoS schema (e.g. SUC, Universal, ON5 Penn Treebank tag sets) and language must be set for each corpus.
+#
+# #### Load DTM
+#
+# <b>To select a corpus:</b> <b>1)</b> press <b>`Change`</b> to open the file browser, <b>2)</b> find and select the file you want to open and <b>3)</b> press <b>`Change`</b> again to confirm the file and close the prowser. Then you can load the corpus by pressing <b>`Load`</b>.
+#
+# #### Word trends
+#
+# Add words of interest and/or regular expressions in the text box. The system will automatically plot matching word - words not plotted does not exist in the (processed) corpus. The regular expressions must be surrounded by vertical bars `|`. To find words ending with `tion` you can enter `|.*tion$|` in the textbox. I might seem cryptical, but is a very powerful notation for searching words. The vertical bars is specified only so that the system can distinguish the regexp from "normal" words. The actual expression is `^.*tion$`. The dot and star`.*` matches any character (the dot) any number of times (the `*`). The dollar sign `$` indicates the word ending. So this expression matches all words that begins with any number of characters follwoed, by the character sequence `tion` at the end of the word. To match all words starting with `info`you can enter `|^info.*|` where `^` specifies the start of the word.
+#
+
 
 # %%
-
-from dataclasses import dataclass
-
-import penelope.notebook.dtm as vectorize_gui
-import penelope.notebook.dtm.load_DTM_gui as load_DTM_gui
-import penelope.notebook.word_trends as word_trends
+# %load_ext autoreload
+# %autoreload 2
 from bokeh.plotting import output_notebook
 from IPython.core.display import display
-from penelope.corpus import VectorizedCorpus
 
-import __paths__
-from notebooks.common.spacy_pipelines import spaCy_DTM_pipeline
-from notebooks.corpus_data_config import SSI, CorpusConfig
+import __paths__  # pylint: disable=unused-import
+from notebooks.word_trends import word_trends_gui
 
 output_notebook()
-
-
-@dataclass
-class State:
-    word_trend_data: word_trends.WordTrendData = None
-
-
-ssi: CorpusConfig = SSI(corpus_folder=__paths__.data_folder)
-state = State(word_trend_data=word_trends.WordTrendData())
-
-
-def corpus_loaded_callback(
-    corpus: VectorizedCorpus, corpus_tag: str, corpus_folder: str
-):  # pylint: disable=unused-argument
-    global state
-    print("Corpus succesfully vectorized!")
-    print("Generating trend data!")
-    state.word_trend_data.update(
-        corpus=corpus.group_by_year(),
-        corpus_folder=corpus_folder,
-        corpus_tag=corpus_tag,
-        n_count=1000,
-    )
-    print("Data successfully created!")
-
-
-# %%
-
-gui_vectorize = vectorize_gui.display_gui(
-    corpus_folder=__paths__.data_folder,
-    corpus_config=ssi,
-    pipeline_factory=spaCy_DTM_pipeline,
-    done_callback=corpus_loaded_callback,
-)
-
-display(gui_vectorize.layout(hide_input=False, hide_output=False))
-
-# %%
-
-gui_load = load_DTM_gui.create_gui(
-    corpus_folder=__paths__.data_folder,
-    loaded_callback=corpus_loaded_callback,
-)
-
-display(gui_load.layout())
-
-# %%
-
-gui_trends = word_trends.create_gui(
-    corpus=state.word_trend_data.corpus,
-    corpus_folder=state.word_trend_data.corpus_folder,
-    corpus_tag=state.word_trend_data.corpus_tag,
-    word_trend_data=state.word_trend_data,
-)
-display(gui_trends.layout())
+display(word_trends_gui.create_gui(corpus_folder=__paths__.data_folder, corpus_config_name="SSI"))
 
 # %%
---
+++ notebooks/word_trends/word_trends_gui.py
@@ -0,0 +1,78 @@
+import ipywidgets as widgets
+import penelope.notebook.dtm.compute_DTM_pipeline as compute_DTM_pipeline
+import penelope.notebook.dtm.load_DTM_gui as load_DTM_gui
+import penelope.notebook.dtm.to_DTM_gui as to_DTM_gui
+import penelope.notebook.word_trends as word_trends
+from IPython.core.display import display
+from penelope.corpus import VectorizedCorpus
+from penelope.pipeline import CorpusConfig
+from penelope.pipeline.spacy.pipelines import spaCy_DTM_pipeline
+
+import __paths__
+
+view = widgets.Output(layout={'border': '2px solid green'})
+
+@view.capture(clear_output=True)
+def corpus_loaded_callback(
+    corpus: VectorizedCorpus,
+    corpus_tag: str,
+    corpus_folder: str,
+    **_,
+):
+    trends_data: word_trends.TrendsData = word_trends.TrendsData(
+        corpus=corpus,
+        corpus_folder=corpus_folder,
+        corpus_tag=corpus_tag,
+        n_count=25000,
+    ).update()
+
+    gui = word_trends.GofTrendsGUI(
+        gofs_gui=word_trends.GoFsGUI().setup(),
+        trends_gui=word_trends.TrendsGUI().setup(),
+    )
+
+    display(gui.layout())
+    gui.display(trends_data=trends_data)
+
+
+@view.capture(clear_output=True)
+def corpus_compute_callback(*args, **kwargs):
+    compute_DTM_pipeline.compute_document_term_matrix(*args, **kwargs)
+
+
+def create_gui(corpus_folder: str, corpus_config_name: str) -> widgets.CoreWidget:
+    config = CorpusConfig.find(corpus_config_name, __paths__.resources_folder).folder(corpus_folder)
+    gui_compute = to_DTM_gui.create_gui(
+        corpus_folder=corpus_folder,
+        corpus_config=config,
+        pipeline_factory=spaCy_DTM_pipeline,
+        compute_document_term_matrix=corpus_compute_callback,
+        done_callback=corpus_loaded_callback,
+    )
+
+    gui_load = load_DTM_gui.create_gui(
+        corpus_folder=corpus_folder,
+        loaded_callback=corpus_loaded_callback,
+    )
+
+    accordion = widgets.Accordion(
+        children=[
+            widgets.VBox(
+                [
+                    gui_load.layout(),
+                ],
+                layout={'border': '1px solid black', 'padding': '16px', 'margin': '4px'},
+            ),
+            widgets.VBox(
+                [
+                    gui_compute.layout(),
+                ],
+                layout={'border': '1px solid black', 'padding': '16px', 'margin': '4px'},
+            ),
+        ]
+    )
+
+    accordion.set_title(0, "LOAD AN EXISTING DOCUMENT-TERM MATRIX")
+    accordion.set_title(1, '...OR COMPUTE A NEW DOCUMENT-TERM MATRIX')
+
+    return widgets.VBox([accordion, view])
