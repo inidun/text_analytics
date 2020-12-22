c0746da45380a4fee241dd7784eac64ac6a68430
Roger Mähler <roger.mahler@hotmail.com>
2020-12-22 10:51:01 +0100

    cosmosmetics

--- notebooks/word_trends/word_trends.py
+++ notebooks/word_trends/word_trends.py
@@ -19,37 +19,38 @@
 #
 # ### Text Processing Pipeline
 #
-# | | Building block | Arguments | Description |
-# | -- | :------------- | :------------- | :------------- |
-# | ⚙ | <b>SetTagger</b>SpacyModel | 'en' | spaCy as PoS tagger
-# | 📜| <b>LoadText</b> | reader_opts, transform_opts | Text stream provider
-# | 🔎 | <b>Tqdm</b> | ⚪ | Progress indicator
-# | ⌛ | <b>Passthrough</b> | ⚪ | Passthrough
-# | 🔨 | Spacy<b>ToTagged</b>Frame | ⚪ | PoS tagging
-# | 💾 | <b>Checkpoint</b> | checkpoint_filename | Checkpoint (tagged frames) to file
-# | 🔨 | TaggedFrame<b>ToTokens</b> | extract_tagged_tokens_opts, filter_opts | Tokens extractor
-# | 🔨 | <b>TokensTransform</b> | tokens_transform_opts | Tokens transformer
-# | 🔨 | <b>ToDocumentContentTuple</b> | ⚪ | Protocol/API adapter
-# | 🔎 | <b>Tqdm</b> | ⚪ | Progress indicator
-# | 🔨 | <b>ToDTM</b> | vectorize_opts| DTM vectorizer
-# | 💾 | <b>Checkpoint</b> | checkpoint_filename| Checkpoint (DTM) to file
+# | 🔽 | Building block | Arguments | Description | Configuration
+# | -- | :------------- | :------------- | :------------- | :------------- |
+# | ⚙ | <b>SetTagger</b>SpacyModel | 'en' | Set PoS tagger | spaCy
+# | 📜| <b>LoadText</b> | reader_opts, transform_opts | Text stream provider | config.yaml
+# | 🔎 | <b>Tqdm</b> | ⚪ | Progress indicator | ⚪
+# | ⌛ | <b>Passthrough</b> | ⚪ | Passthrough | ⚪
+# | 🔨 | <b>ToTaggedFrame</b> | ⚪ Spacy | PoS tagging | config.yaml
+# | 💾 | <b>Checkpoint</b> | checkpoint_filename | Checkpoint (tagged frames) to file | ⚪
+# | 🔨 | TaggedFrame<b>ToTokens</b> | extract_tagged_tokens_opts, filter_opts | Tokens extractor | User specified
+# | 🔨 | <b>TokensTransform</b> | tokens_transform_opts | Tokens transformer | User specified
+# | 🔨 | <b>ToDocumentContentTuple</b> | ⚪ | API adapter | ⚪
+# | 🔎 | <b>Tqdm</b> | ⚪ | Progress indicator | ⚪
+# | 🔨 | <b>ToDTM</b> | vectorize_opts| DTM vectorizer | User specified
+# | 💾 | <b>Checkpoint</b> | checkpoint_filename| Checkpoint (DTM) to file | User specified
 #
 # ### User instructions
 #
 # #### Compute DTM
 #
-# This notebook implements the entire processing pipeline from plain text to computed (and stored)
-# document-term matrix that are the basis for the word trend exploration.
+# This notebook implements the entire processing pipeline from plain text to a computed (and stored)
+# document-term matrix (DTM) that are the basis for the word trend exploration.
 #
 # For large corpora the DTM processing time can be considerable and in such a case you
 # should consider using the CLI-version of the processing pipeline.
 #
 # Note that the computed DTM is saved on disk in the specified folder. You must enter a
-# tag that will be used when namin the (principel) result data file. This file will be named
-# tag + `_vectorized_data.pickle` and uniquely identifies the bundle of files that makes
-# up the result. Note that if the `tag` already exists in specified folder then it will will
-# be overwritten. The tag can be used to describe the main compute arguments. If the
-# The result files will be stored in a subfolder named with the tag if the `Create folder` option is checked.
+# tag that will be used when naming the (principal) result data file. This file will be named
+# "tag + `_vectorized_data.pickle`" and will be used to uniquely identify the bundle of files that makes
+# up the result. Note that if the `tag` already exists in the specified target folder then it will
+# be overwritten. You can use the tag to describe the arguments of the computation i.e. PoS tags etc. If the `Create folder` option is checked, then the result bundle will be stored in a subfolder of the target folder named _tag_.
+#
+# #### Compute arguments
 #
 # | | Config element |  Description |
 # | -- | :------------- | :------------- |
@@ -62,11 +63,11 @@
 # | | Extra stopwords | Additional stopwords
 # | | Filename fields | Specifies attribute values to be extracted from filenames
 #
-# N.B. Note that PoS schema (e.g. SUC, Universal, ON5 Penn Treebank tag sets) and language must be set for each corpus.
+# N.B. Note that PoS schema (e.g. SUC, Universal, ON5 Penn Treebank tag sets) and language must be set for each corpus. This, and other options, is specified in the _corpus configuration file_. For an example, please see _SSI.yaml_ inf the `resources` folder.
 #
-# #### Load DTM
+# #### Load a DTM corpus
 #
-# <b>To select a corpus:</b> <b>1)</b> press <b>`Change`</b> to open the file browser, <b>2)</b> find and select the file you want to open and <b>3)</b> press <b>`Change`</b> again to confirm the file and close the prowser. Then you can load the corpus by pressing <b>`Load`</b>.
+# To load an existing corpus youmust first select a file, then press <b>`Load`</b>. To select the file:</b> <b>1)</b> press <b>`Change`</b> to open the file browser, <b>2)</b> find and select the file you want to open and <b>3)</b> press <b>`Change`</b> again to confirm the file and close the file browser. Then you can load the corpus by pressing <b>`Load`</b>.
 #
 # #### Word trends
 #
@@ -75,8 +76,6 @@
 
 
 # %%
-# %load_ext autoreload
-# %autoreload 2
 from bokeh.plotting import output_notebook
 from IPython.core.display import display
 
@@ -85,5 +84,3 @@ from notebooks.word_trends import word_trends_gui
 
 output_notebook()
 display(word_trends_gui.create_gui(corpus_folder=__paths__.data_folder, corpus_config_name="SSI"))
-
-# %%
