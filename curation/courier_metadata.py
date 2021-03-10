e4e984a65ad280e924f4b491c1bb7303e89880c6
aibakeneko <aibakeneko@gmail.com>
2021-03-10 17:27:40 +0100

    added notes

--- curation/courier_metadata.py
+++ curation/courier_metadata.py
@@ -1,15 +1,16 @@
 # %%
 
-from typing import List
-import re
-import pandas as pd
 import itertools
+import glob
+import os
+import re
+from typing import List
 
-from pytz import AmbiguousTimeError
+import pandas as pd
 
 pd.set_option('display.max_columns', None)
 pd.set_option('max_colwidth', None)
-pd.set_option("max_rows", None)
+pd.set_option("max_rows", 5)
 # %%
 
 
@@ -32,6 +33,29 @@ def expand_article_pages(page_ref: str) -> List[int]:
     return list(ix)
 
 
+def extract_courier_id(eng_host_item: str) -> str:
+
+    m = re.match(r'.*\s(\d+\seng$)', eng_host_item)
+    if not m:
+        print(eng_host_item)
+        return None
+
+    courier_id = m.group(1).replace(' ', '')
+    if len(courier_id) < 9:
+        courier_id = '0' + courier_id
+
+    return courier_id
+
+
+def extract_english_host_item(host_item: str) -> str:
+
+    items = [x for x in host_item.split("|") if x.endswith('eng')]
+    if len(items) == 0:
+        return None
+
+    return items[0]
+
+
 def create_article_index(filename: str) -> pd.DataFrame:
 
     df = pd.read_csv(filename, sep=';')
@@ -51,61 +75,77 @@ def create_article_index(filename: str) -> pd.DataFrame:
     ]
 
     df = df[df['document_type'] == 'article']
+    df = df[df.languages.str.contains('eng')]
 
-    df['eng_host_item'] = df['host_item'].apply(lambda x: x.split('|')[0])
+    df['eng_host_item'] = df['host_item'].apply(extract_english_host_item)
+    df = df.copy()
+
     df['page_ref'] = df['eng_host_item'].str.extract(
         r'((?:p\.\,?|pages?)(?:\s*\d+(?:-\d+)*)(?:\,\s*\d{1,3}(?:-\d{1,3})*\s)*)'
     )[0]
     df.loc[df.record_number == 187812, 'page_ref'] = 'p. 18-31'
     df.loc[df.record_number == 64927, 'page_ref'] = 'p. 28-29'
-    df['courier_id'] = df['eng_host_item'].str.extract(r'(\s\d+\seng$)').replace(' ', '')
+    df['courier_id'] = df.eng_host_item.apply(extract_courier_id)
 
     df['pages'] = df.page_ref.apply(expand_article_pages)
     df['year'] = df.publication_date.apply(lambda x: int(x[:4]))
 
-    return df[[
-        'record_number',
-        'catalogue_title',
-        # 'authors',
-        # 'titles_in_other_languages',
-        # 'languages',
-        # 'series',
-        # 'catalogue_subjects',
-        # 'document_type',
-        # 'host_item',
-        'eng_host_item',
-        'courier_id',
-        'year',
-        'publication_date',
-        'pages',
-        'notes',
-    ]]
+    return df[
+        [
+            'record_number',
+            'catalogue_title',
+            # 'authors',
+            # 'titles_in_other_languages',
+            # 'languages',
+            # 'series',
+            # 'catalogue_subjects',
+            # 'document_type',
+            # 'host_item',
+            'eng_host_item',
+            'courier_id',
+            'year',
+            'publication_date',
+            'pages',
+            'notes',
+        ]
+    ]
 
+
+article_index = create_article_index('UNESCO_Courier_metadata.csv')
+# article_index.to_csv('UNESCO_Courier_articles_metadata.csv', sep='\t')
+
+# %%
 def extract_article_pages():
     pass
 
-def extract_articles(folder: str, article_index: pd.DataFrame) -> None:
-    
-    for x in article_index.iteritems():
 
-        filename_pattern = os.path.join(folder, f"{x['courier_id']}*.txt")
+def extract_articles(folder: str, article_index: pd.DataFrame) -> None:
+
+    for _, x in article_index.iterrows():
+
+        filename_pattern = os.path.join(folder, f"{x['courier_id']}*.xml")
         filenames = glob.glob(filename_pattern)
 
         if len(filenames) == 0:
-            print('no match')
+            print(x['courier_id'], 'no match')
             continue
 
         if len(filenames) > 1:
-            print("AmbiguousTimeError")
+            print(x['courier_id'], "AmbiguousTimeError")
             continue
 
-        extract_article_pages(filenames[0], x['pages'], x['title'])
+        # print(filenames[0])
 
-df = create_article_index('UNESCO_Courier_metadata.csv')
-df.to_csv('UNESCO_Courier_articles_metadata.csv', sep='\t')
+        # TODO: Check pages for possible mismatch
+        # TODO: Check overlapping pages
+        # extract_article_pages(filenames[0], x['pages'], x['title'])
+
+
+extract_articles("./data/xml", article_index)
+
 
 # %%
-#import ast
-#ast.literal_eval('[0,1,2,3,4 5]'
+# import ast
+# ast.literal_eval('[0,1,2,3,4 5]'
 
 # %%
---
+++ curation/notes.md
@@ -0,0 +1,11 @@
+## PDFBox Uuable to extract text from
+132107engb
+
+## In index but missing file
+059709eng no match
+124785eng no match
+367693eng no match
+370032eng no match
+370977eng no match
+372603eng no match
+373788eng no match
