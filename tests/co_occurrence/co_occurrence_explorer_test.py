6b8e7757711af28cf763bc399a8abfad55f8457b
Roger Mähler <roger.mahler@hotmail.com>
2021-10-01 15:54:19 +0200

    Disable tests

--- tests/co_occurrence/co_occurrence_explorer_test.py
+++ tests/co_occurrence/co_occurrence_explorer_test.py
@@ -1,24 +1,25 @@
-import ipywidgets as widgets
-import penelope.co_occurrence as co_occurrence
-import penelope.notebook.co_occurrence as explore_gui
-from penelope.notebook.co_occurrence import main_gui
+# import ipywidgets as widgets
+# # import penelope import co_occurrence
+# # import penelope.notebook.co_occurrence as explore_gui
+# # from penelope.notebook.co_occurrence import main_gui
 
-view = widgets.Output(layout={'border': '2px solid green'})
-
-
-def test_create_co_occurrence_explorer_gui():
-
-    corpus_filename: str = './tests/test_data/VENUS/VENUS_co-occurrence.csv.zip'
-    bundle: co_occurrence.Bundle = co_occurrence.Bundle.load(corpus_filename, compute_frame=False)
-
-    trends_data = main_gui.to_trends_data(bundle).update()
-    gui_explore: explore_gui.ExploreGUI = explore_gui.ExploreGUI(bundle=bundle).setup().display(trends_data=trends_data)
-
-    assert gui_explore is not None
+# view = widgets.Output(layout={'border': '2px solid green'})
 
 
-def generic_patch(return_value):
-    def _generic_patch(*x, **y):  # pylint: disable=unused-argument
-        return return_value
+# # FIXME Penelope only test, remove or move
+# # def test_create_co_occurrence_explorer_gui():
 
-    return _generic_patch
+# #     corpus_filename: str = './tests/test_data/VENUS/VENUS_co-occurrence.csv.zip'
+# #     bundle: co_occurrence.Bundle = co_occurrence.Bundle.load(corpus_filename, compute_frame=False)
+
+# #     trends_data = main_gui.to_trends_data(bundle).update()
+# #     gui_explore: explore_gui.ExploreGUI = explore_gui.ExploreGUI(bundle=bundle).setup().display(trends_data=trends_data)
+
+# #     assert gui_explore is not None
+
+
+# def generic_patch(return_value):
+#     def _generic_patch(*x, **y):  # pylint: disable=unused-argument
+#         return return_value
+
+#     return _generic_patch
--- tests/co_occurrence/co_occurrence_gui_test.py
+++ tests/co_occurrence/co_occurrence_gui_test.py
@@ -1,10 +1,9 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 import ipywidgets as widgets
-import penelope.notebook.word_trends as word_trends_gui
-from penelope import co_occurrence, pipeline
+from penelope import pipeline
 from penelope.notebook import interface
-from penelope.notebook.co_occurrence import explore_co_occurrence_gui, main_gui
+from penelope.notebook.co_occurrence import main_gui
 
 view = widgets.Output(layout={'border': '2px solid green'})
 
@@ -27,32 +26,32 @@ def test_compute_co_occurrence_callback():
     )
 
 
-@patch('penelope.co_occurrence.to_trends_data', lambda _: Mock(spec=word_trends_gui.BundleTrendsData))
-@patch(
-    'penelope.notebook.co_occurrence.ExploreGUI',
-    lambda: Mock(spec=explore_co_occurrence_gui.ExploreGUI, **{'setup': Mock}),
-)
-def test_create_MainGUI():
-    corpus_folder: str = './tests/test_data'
-    config: pipeline.CorpusConfig = pipeline.CorpusConfig.load('./tests/test_data/SSI.yml')
-    gui = main_gui.MainGUI(
-        corpus_config=config, data_folder=corpus_folder, corpus_folder=corpus_folder, resources_folder=corpus_folder
-    )
-    layout = gui.layout()
-    assert layout is not None
+# @patch('penelope.co_occurrence.to_trends_data', lambda _: Mock(spec=word_trends_gui.BundleTrendsData))
+# @patch(
+#     'penelope.notebook.co_occurrence.ExploreGUI',
+#     lambda: Mock(spec=explore_co_occurrence_gui.ExploreGUI, **{'setup': Mock}),
+# )
+# def test_create_MainGUI():
+#     corpus_folder: str = './tests/test_data'
+#     config: pipeline.CorpusConfig = pipeline.CorpusConfig.load('./tests/test_data/SSI.yml')
+#     gui = main_gui.MainGUI(
+#         corpus_config=config, data_folder=corpus_folder, corpus_folder=corpus_folder, resources_folder=corpus_folder
+#     )
+#     layout = gui.layout()
+#     assert layout is not None
 
-    gui.display_explorer(bundle=Mock(spec=co_occurrence.Bundle))
+#     gui.display_explorer(bundle=Mock(spec=co_occurrence.Bundle))
 
 
-@view.capture(clear_output=True)
-def test_create_co_occurrence_explorer_gui():
+# @view.capture(clear_output=True)
+# def test_create_co_occurrence_explorer_gui():
 
-    corpus_filename: str = co_occurrence.to_filename(folder='./tests/test_data/VENUS', tag='VENUS')
-    bundle: co_occurrence.Bundle = co_occurrence.Bundle.load(corpus_filename, compute_frame=False)
+#     corpus_filename: str = co_occurrence.to_filename(folder='./tests/test_data/VENUS', tag='VENUS')
+#     bundle: co_occurrence.Bundle = co_occurrence.Bundle.load(corpus_filename, compute_frame=False)
 
-    trends_data = main_gui.to_trends_data(bundle).update()
-    gui_explore: explore_co_occurrence_gui.ExploreGUI = (
-        explore_co_occurrence_gui.ExploreGUI(bundle).setup().display(trends_data=trends_data)
-    )
+#     trends_data = main_gui.to_trends_data(bundle).update()
+#     gui_explore: explore_co_occurrence_gui.ExploreGUI = (
+#         explore_co_occurrence_gui.ExploreGUI(bundle).setup().display(trends_data=trends_data)
+#     )
 
-    assert gui_explore is not None
+#     assert gui_explore is not None
