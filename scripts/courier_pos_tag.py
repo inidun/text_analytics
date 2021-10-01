e754eb4818fd91bcdd16f2c193efaecb6768b005
Roger Mähler <roger.mahler@hotmail.com>
2021-10-01 23:23:59 +0200

    Handle removal of eol hyphens

--- scripts/courier_pos_tag.py
+++ scripts/courier_pos_tag.py
@@ -4,7 +4,7 @@ import re
 import click
 import spacy
 from penelope.corpus import TextTransformOpts
-from penelope.pipeline import CorpusConfig, CorpusPipeline, DEFAULT_TAGGED_FRAMES_FILENAME_SUFFIX
+from penelope.pipeline import DEFAULT_TAGGED_FRAMES_FILENAME_SUFFIX, CorpusConfig, CorpusPipeline
 from penelope.utility import path_add_suffix
 from spacy.language import Language
 
@@ -30,15 +30,14 @@ def main(config_filename: str = None):
     en_nlp: Language = spacy.load(os.path.join(os.environ.get("SPACY_DATA", ""), "en_core_web_sm"))
 
     text_transform_opts = TextTransformOpts(
-        fix_hyphenation=False,
-        fix_whitespaces=True,
-        fix_accents=True,
-        extra_transforms=[remove_hyphens]
+        fix_hyphenation=False, fix_whitespaces=True, fix_accents=True, extra_transforms=[remove_hyphens]
     )
 
     attributes = ['text', 'lemma_', 'pos_']
     config: CorpusConfig = CorpusConfig.load(path=config_filename)
-    tagged_frames_filename: str = os.path.abspath(path_add_suffix(config.pipeline_payload.source, DEFAULT_TAGGED_FRAMES_FILENAME_SUFFIX))
+    tagged_frames_filename: str = os.path.abspath(
+        path_add_suffix(config.pipeline_payload.source, DEFAULT_TAGGED_FRAMES_FILENAME_SUFFIX)
+    )
 
     pipeline = (
         CorpusPipeline(config=config)
